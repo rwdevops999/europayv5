a4147fa79d85edec3c16023af47bfe066bda0cb4
rwdevops999 <rwdevops999@gmail.com>
2025-08-22 16:18:19 +0200

    removed
    red border

--- app/(europay)/layout.tsx
+++ app/(europay)/layout.tsx
@@ -17,7 +17,7 @@ const EuropayLayout = ({ children }: EuropayLayoutProps) => {
     <div className="border-border/40 dark:border-border">
       <div className="mx-auto w-full border-border/40 dark:border-border min-[2000px]:max-w-[1536px] min-[2000px]:border-x">
         <div className="flex-1">
-          <div className="h-screen border-1 border-red-500">
+          <div className="h-screen">
             <Navbar />
             <div style={{ height: dynamicHeight }}>{children}</div>
             <Footer />
